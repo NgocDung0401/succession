bf87a64fe5b6ded65de2be327e494258be050e2f
Ngoc Dung <seehafer@gmail.com>
2024-09-26 01:41:11 +0700

    Remove NHShop-Project

--- NHShop-Project/src/routers/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { forgotPassword, signin, signup } from "../controllers/auth";
-
-const router = express.Router();
-
-router.post(`/auth/signup`, signup);
-router.post(`/auth/signin`, signin);
-router.post(`/auth/forgot`, forgotPassword);
-export default router;
